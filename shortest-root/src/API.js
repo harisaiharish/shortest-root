6912fbd840be35bb39220828224c402fdf310247
harisaiharish <harisaiharish@gmail.com>
2024-01-21 01:42:22 +0530

    hari joins

--- shortest-root/src/API.js
+++ shortest-root/src/API.js
@@ -1,6 +1,6 @@
 API_KEY = "AIzaSyC-8yJHf4WGRxTvRDMaRtYos7wxkE6C86A"
 
-NEARBY_SEARCH = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?keyword=cruise&location=-33.8670522%2C151.1957362&radius=1500&type=restaurant&key=AIzaSyC-8yJHf4WGRxTvRDMaRtYos7wxkE6C86A"
+NEARBY_SEARCH = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?keyword=cruise&location=-33.8670522, 151.1957362&radius=1500&type=restaurant&key=AIzaSyC-8yJHf4WGRxTvRDMaRtYos7wxkE6C86A"
 console.log("HIIIIII")
 
 function setAPIcall(keyword, lat, long, radius = 50000){
--- shortest-root/src/Pages/Maps.js
+++ shortest-root/src/Pages/Maps.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { GoogleMap, useLoadScript, Marker } from '@react-google-maps/api';
 
 const Maps = () => {
@@ -9,17 +9,55 @@ const Maps = () => {
       };
 
     const libraries = ['places'];
-    const { isLoaded, loadError } = useLoadScript({
-        googleMapsApiKey: 'AIzaSyC-8yJHf4WGRxgitTvRDMaRtYos7wxkE6C86A',
-        libraries,
-      });
+    // const { isLoaded, loadError } = useLoadScript({
+    //     googleMapsApiKey: 'AIzaSyC-8yJHf4WGRxTvRDMaRtYos7wxkE6C86A',
+    //     libraries,
+    //   });
+
+    const [lat, setLat] = useState(34.89)
+    const [lng, setLng] = useState(45.67)
+    const [showAvg, setShow] = useState(false)
+    const [avgLat, setAvgLat] = useState(0)
+    const [avgLng, setAvgLng] = useState(0)
+
+    const arr = [
+        {
+            lat: 34.89,
+            lng: 45.67
+        },
+        {
+            lat: 35.89,
+            lng: 45.67
+        },
+        {
+            lat: 35.89,
+            lng: 46.67
+        }
+    ]
+
+    const avgLocation = () => {
+        var sumLat = 0
+        var sumLng = 0
+        arr.forEach(element => {
+            sumLat = sumLat + element.lat
+            sumLng = sumLng + element.lng
+        })
+        setAvgLat(sumLat/(arr.length))
+        setAvgLng(sumLng/(arr.length))
+        setShow(true)
+    }
 
     return (
 
         <div>
-            <input />
-            <GoogleMap center={{lat: 34.89, lng: 45.67}} id='map' mapContainerStyle={mapContainerStyle} zoom={8}>
-                <Marker position={{lat: 34.89, lng: 45.67}}/>
+            <button onClick={avgLocation} style={{width: "100px", height: "20px"}}></button>
+            <GoogleMap center={{lat: lat, lng: lng}} id='map' mapContainerStyle={mapContainerStyle} zoom={8}>
+                {arr.map(element => (
+                    <Marker position={{lat: element.lat, lng: element.lng}}/>
+                ))}
+                {showAvg && (
+                    <Marker position={{lat: avgLat, lng: avgLng}}/>
+                )}
             </GoogleMap>
         </div>
     )
