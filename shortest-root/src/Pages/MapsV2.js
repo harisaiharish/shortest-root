659d8adb74545301c90a83bbcc8a00edb8ec6d4a
Aniketh Ganta <aniganta7@gmail.com>
2024-01-20 20:31:36 +0530

    Final update before tabbing off

---
+++ shortest-root/src/Pages/MapsV2.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { GoogleMap, useLoadScript, Marker } from '@react-google-maps/api';
+//import { Route, Switch, BrowserRouter } from 'react-router-dom';
+
+const libraries = ['places'];
+const mapContainerStyle = {
+  width: '100vw',
+  height: '100vh',
+};
+const center = {
+  lat: 7.2905715, // default latitude
+  lng: 80.6337262, // default longitude
+};
+
+const MapsV2 = () => { 
+  const { isLoaded, loadError } = useLoadScript({
+    googleMapsApiKey: 'AIzaSyC-8yJHf4WGRxTvRDMaRtYos7wxkE6C86A',
+    libraries,
+  });
+
+  if (loadError) {
+    return <div>Error loading maps</div>;
+  }
+
+  if (!isLoaded) {
+    return <div>Loading maps</div>;
+  }
+
+  return (
+    <div>
+      <GoogleMap
+        mapContainerStyle={mapContainerStyle}
+        zoom={10}
+        center={center}
+      >
+        <Marker position={center} />
+      </GoogleMap>
+    </div>
+  );
+};
+
+export default MapsV2;
--- shortest-root/src/Pages/map.js
+++ shortest-root/src/Pages/map.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { GoogleMap, useLoadScript, Marker } from '@react-google-maps/api';
 import { Route, Switch, BrowserRouter } from 'react-router-dom';
 
+const user_Locations = [[12.9235, 77.6851], [12.8862, 77.7459], [12.9722, 77.6843], [12.9273, 77.7051]]
+
 const libraries = ['places'];
 const mapContainerStyle = {
   width: '100vw',
@@ -12,7 +14,15 @@ const center = {
   lng: 80.6337262, // default longitude
 };
 
-const App = () => { 
+window.onload = function () {
+  console.log("HEY THERE")
+  const gMap = document.getElementById('map');
+  var newMarker = document.createElement('Marker');
+  newMarker.setAttribute("position", { lat: 0, lng: 0 });
+  gMap.appendChild(newMarker);
+};
+
+const Map = () => { 
   const { isLoaded, loadError } = useLoadScript({
     googleMapsApiKey: 'AIzaSyC-8yJHf4WGRxTvRDMaRtYos7wxkE6C86A',
     libraries,
@@ -29,6 +39,7 @@ const App = () => {
   return (
     <div>
       <GoogleMap
+        id = 'map'
         mapContainerStyle={mapContainerStyle}
         zoom={10}
         center={center}
@@ -39,4 +50,23 @@ const App = () => {
   );
 };
 
-export default App;
+export default Map;
+/*
+  return (
+    <div>
+      <GoogleMap
+        mapContainerStyle={mapContainerStyle}
+        zoom={10}
+        center={center}
+      >
+        {
+        user_Locations.map((element) => (
+          <Marker position={ {"lat":element[0], "lng":element[1]} } />
+        ))}
+      </GoogleMap>
+    </div>
+  );
+};
+*/
+
+
