35398e0ca19e28e8bc3e04d82d0b9bf0d785baa2
Adityasundar <adiblaze1401@gmail.com>
2024-01-20 11:41:03 +0530

    -m

--- shortest-root/src/App.js
+++ shortest-root/src/App.js
@@ -1,19 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
-import { GoogleOAuthProvider } from '@react-oauth/google';
-import Google from './google.tsx';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <GoogleOAuthProvider clientId='http://524566069960-psogbhe0f8hohv6rm4b2slvv4gdp2qno.apps.googleusercontent.com/'>
-          <Google />
-        </GoogleOAuthProvider>
-      </header>
-    </div>
-  );
+import './App.css';
+import {Route, Switch, BrowserRouter} from 'react-router-dom';
+import Home from './Pages/Home.js';
+import {React, Component} from 'react';
+
+export class App extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <div>
+          <Switch>
+            <Route exact path='/' component={() => (
+              <Home />
+            )} />
+          </Switch>
+        </div>
+      </BrowserRouter>
+    );
+  }
 }
 
 export default App;
---
+++ shortest-root/src/Pages/Home.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import '../App.css'
+
+export class Home extends Component {
+  render() {
+    return (
+      <div>
+        <h1>
+            Hello
+        </h1>
+      </div>
+    )
+  }
+}
+
+export default Home
