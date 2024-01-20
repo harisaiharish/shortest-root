8296e769cc8be16a973b0f5fcc40d6b0a00c5062
Adityasundar <adiblaze1401@gmail.com>
2024-01-20 11:15:57 +0530

    commit

--- shortest-root/src/App.js
+++ shortest-root/src/App.js
@@ -1,22 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import { GoogleOAuthProvider } from '@react-oauth/google';
+import Google from './google.tsx';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <GoogleOAuthProvider clientId='http://524566069960-psogbhe0f8hohv6rm4b2slvv4gdp2qno.apps.googleusercontent.com/'>
+          <Google />
+        </GoogleOAuthProvider>
       </header>
     </div>
   );
